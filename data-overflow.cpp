30cf3466e6711a86c52ca440f132f80dfd9aef54
nazmul-1117 <nazmulonline2002@gmail.com>
2024-06-25 11:46:52 +0600

    add more

---
+++ data-overflow.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int32_t main (){
+  int a;
+  int b;
+
+  a = INT_MAX;
+  b = INT_MAX;
+
+  // cout << a*b << endl;
+  cout << a * 1LL * b << endl;
+
+//   cout << a + b << endl;
+  cout << a + 0LL + b << endl;
+
+  return 0;
+}
